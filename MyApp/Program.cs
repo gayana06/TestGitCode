c8c9030d5472b9980e2439c10b2cbfc3e2923a43
gayana06 <gayanachandrasekara@gmail.com>
2016-08-19 10:24:52 +0200

    final changes

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -10,6 +10,7 @@ namespace MyApp
             Console.WriteLine("New line");
             Console.WriteLine("New line added by ui");
             Console.WriteLine("added by feature2 modifiedby feature2");
+            Console.WriteLine("final by feature2");
         }
     }
 }
